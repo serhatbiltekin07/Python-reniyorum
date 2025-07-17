8eda1610cb5f03e7e021ca5a67b4787f24f9cc2a
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-07-17 12:38:39 +0300

    Add files via upload

---
+++ veri-tipleri.py
@@ -0,0 +1,11 @@
+isim = "Çınar"
+yas = 7
+kilo = 15.5
+ogrenciMi = True
+
+print(type(isim))  # str
+print(type(yas))   # int 
+print(type(kilo))  # float
+print(type(ogrenciMi))  #bool
+
+print("ad: " + isim + " yaş: " + str(yas) + " kilo: "+str(kilo)+ " öğrenci mi: " + str(ogrenciMi))
