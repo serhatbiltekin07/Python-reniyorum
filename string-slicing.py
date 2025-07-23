6c47f8261065ea438fc71b0ac06329d495f3462e
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-07-23 16:08:10 +0300

    Add files via upload

---
+++ string-slicing.py
@@ -0,0 +1,17 @@
+kurs = "Python ile Programlama"
+
+print(kurs[0])              #p
+print(kurs[-1])             #a
+
+
+adet = len(kurs)            #22
+print(adet)
+print(kurs[adet -1])        # son karakteri bulur = "a"yazar.
+
+print(kurs[0:5])            #Pytho
+print(kurs[0:6])            #Python
+print(kurs[:6])             #Python
+print(kurs[11:22])          #Programlama
+print(kurs[8:len(kurs)])    #le Programlama
+print(kurs[5:])             #n ile Programlama
+print(kurs[::-1])           # ters çevirdi : amalmargorP eli nohtyP
