8ab993e188358819b8bc04a57d648836b8a2065e
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-08-11 01:29:13 +0300

    Add files via upload

---
+++ aritmetik-operatorleri.py
@@ -0,0 +1,10 @@
+a = 30
+b = 20
+sonuc = a + b   #50
+sonuc = a - b   #10
+sonuc = a * b   #600
+sonuc = a / b   #1.5
+sonuc = a % b   #10
+sonuc = a ** b   #348678440100000000000000000000 ==> 30 üzeri 20 dir.
+sonuc = a // b  #1 30 ' un içerisinde 20 bir defa vardır ==> 1
+print(sonuc)
