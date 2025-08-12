b382eeb64b37fe40bc07f2f5bc5950eb06c14661
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-08-12 21:08:30 +0300

    Add files via upload

---
+++ atama-operatorleri.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = 30
+
+#a,b,c = 10,20,(30,40)   # tek satırda kısa bir şekilde atama yaparız #10 20 (30, 40)
+
+#a = a+5  #15 20 30
+a += 5    # a = a+5
+a -= 5    # a = a-5
+a *= 5    # a = a*5
+a /= 5    # a = a/5
+a %= 5    # a = a % 5
+a **= 5    # a = a ** 5 üssünü alır
+a //= 5
+print(a,b,c)    
