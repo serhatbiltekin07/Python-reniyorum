0aa72b74ed16499d7730e58fd9d3269a96a6f3b5
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-08-27 11:25:11 +0300

    Add files via upload

---
+++ if-elseif.py
@@ -0,0 +1,23 @@
+email = "info@biltekinkurtulus.com"
+parola = "12345"
+
+if(email != "info@biltekinkurtulus.com"):
+    print("email yanlış")
+elif(parola != "12345"):
+    print("parola yanlış")
+else:
+    print("Login olundu")
+
+
+#soru
+#soru
+#soru
+#diğer
+
+
+x = 10
+y = 20
+if(x < y):
+    print("x y den küçük")  #x y den küçük
+else:
+    print("y x den küçük")
