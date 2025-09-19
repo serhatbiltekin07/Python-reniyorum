896790b1d5134d04c3789c3d04a54b523748efc4
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2025-09-19 23:13:30 +0300

    Add files via upload

---
+++ fonksiyon-tanimlama.py
@@ -0,0 +1,14 @@
+def selamlama():
+    for i in range(10):
+        print("Merhaba Akademi")
+
+
+#selamlama() #Merhaba Akademi  10 defa yazılır
+
+
+def toplama():
+    a = 10
+    b = 20
+    print(a+b)
+
+toplama()   #30
